e539789bd78aa3b66d9ecc2e419849c20165cc15
dopeelis <77695420+dopeelis@users.noreply.github.com>
2021-09-30 12:03:00 +0300

    Delete 04_tab.py

--- 04_tab.py
+++
@@ -1,8 +0,0 @@
-import fileinput
-
-
-def replace_tab_space(file, old_symbol, new_symbol):
-    with fileinput.input(file, inplace=True) as f:
-        for line in f:
-            new_line = line.replace(old_symbol, new_symbol)
-            print(new_line, end='')
